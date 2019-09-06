623387dd085c79f08e11ceb22ada3035fc3cd2e8
Dan <dnshaw12@gmail.com>
2019-09-05 20:09:40 -0500

    prep team can now add ingredients to list

--- src/ManageIngredients/IngredientInfo/index.js
+++ src/ManageIngredients/IngredientInfo/index.js
@@ -3,23 +3,44 @@ import { Segment, Button, Card } from 'semantic-ui-react';
 
 const IngredientInfo = (props) => {
 
+	const inStock = props.ingredient.inStock ? 
+		<input 
+			type='checkbox' 
+			id='inStock'
+			onClick={props.updateIngredient.bind(null, props.ingredient._id)} 
+			checked
+		/> 
+		: 
+		<input 
+			type='checkbox' 
+			id='inStock' 
+			onClick={props.updateIngredient.bind(null, props.ingredient._id)} 
+		/> 
+	const archived = props.ingredient.archived ? 
+		<input 
+			type='checkbox' 
+			id='archived'
+			onClick={props.updateIngredient.bind(null, props.ingredient._id)}  
+			checked
+		/> 
+		: 
+		<input 
+			type='checkbox' 
+			id='archived' 
+			onClick={props.updateIngredient.bind(null, props.ingredient._id)} 
+		/> 
+
 	return(
 
 		<Card>
 			<Card.Content>
 				<Card.Header>{props.ingredient.name}</Card.Header>
 				<p>type: {props.ingredient.type}</p>
-				<p>In Stock: <input 
-					type='checkbox' 
-					id='inStock' 
-					// {props.ingredient.inStock ? 'checked' : null}
-					/>
+				<p>In Stock: 
+					{inStock}
 				</p>
-				<p>Archived: <input 
-					type='checkbox' 
-					id='archived' 
-					// {props.ingredient.archived ? 'checked' : null}
-					/>
+				<p>Archived:  
+					{archived}
 				</p>
 			</Card.Content>
 		</Card>
--- src/ManageIngredients/index.js
+++ src/ManageIngredients/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Button, Card } from 'semantic-ui-react';
+import { Segment, Button, Card, Form } from 'semantic-ui-react';
 
 import IngredientInfo from './IngredientInfo'
 
@@ -9,7 +9,14 @@ class ManageIngredients extends Component {
 		super()
 
 		this.state = {
-			ingredients: null
+			addOpen: false,
+			ingredients: null,
+			name: '',
+			type: 'noodle',
+			vegitarian: false,
+			vegan: false,
+			price: 0,
+			image: {}
 		}
 	}
 
@@ -38,7 +45,7 @@ class ManageIngredients extends Component {
 	}
 
 	sortByType = (a, b) => {
-		
+
 		const typeA = a.type
 		const typeB = b.type
 
@@ -57,24 +64,146 @@ class ManageIngredients extends Component {
 		return comparison
 	}
 
+	updateIngredient = async (id, e) => {
+		
+		console.log(e.target.checked, e.target.id, id);
+
+		const updatedIngredientResponse = await fetch(process.env.REACT_APP_BACKEND_URL + '/ingredients/' + id,{
+					method: 'PUT',
+		        	credentials: 'include',
+		        	body: JSON.stringify({[e.target.id]: e.target.checked}),
+		        	headers: {
+		         	'Content-Type': 'application/json'
+	       		}
+				})
+
+		const parsedResponse = await updatedIngredientResponse.json()
+
+		console.log(parsedResponse);
+
+		const newIngredientList = this.state.ingredients
+
+		newIngredientList[newIngredientList
+			.findIndex( ingredient => ingredient._id === id)] = parsedResponse.data
+
+		this.setState({
+			ingredients: newIngredientList
+		})
+
+	}
+
+	handleChange = (e) => {
+
+		console.log(this.state);
+
+		if (e.target.name === 'vegitarian' || e.target.name === 'vegan') {
+				this.setState({[e.target.name]: e.target.checked})
+		} else if (e.target.name === 'image') {
+			this.setState({image: e.target.files[0]})
+		} else {
+			this.setState({[e.target.name]: e.target.value})
+		}
+
+
+	}
+
+	createIngredient = async (e) => {
+		e.preventDefault()
+		console.log(this.state);
+
+		const data = new FormData();
+      data.append('name', this.state.name);
+      data.append('type', this.state.type);
+      data.append('vegitarian', this.state.vegitarian);
+      data.append('phoneNumber', this.state.phoneNumber);
+      data.append('vegan', this.state.vegan);
+      data.append('price', this.state.price);
+      data.append('image', this.state.image);
+
+		const newIngredientResponse = await fetch(process.env.REACT_APP_BACKEND_URL + '/ingredients/',{
+					method: 'POST',
+		        	credentials: 'include',
+		        	body: data,
+		        	headers: {
+		         	'enctype': 'multipart/form-data'
+	       		}
+				})
+
+		const parsedResponse = await newIngredientResponse.json()
+
+		const newIngredientList = this.state.ingredients
+
+		newIngredientList.push(parsedResponse.data)
+
+		newIngredientList.sort(this.sortByType)
+
+		this.setState({ingredients: newIngredientList})
+
+	}
+
+	toggleAdd = () => {
+		this.setState({addOpen:!this.state.addOpen})
+	}
+
 
 	render(){
+
+		console.log(this.state);
 
 		let ingredientList = <h1>ManageIngredients</h1>
 
 		if (this.state.ingredients) {
 
 			ingredientList = this.state.ingredients.map( ingredient => {
-				return <IngredientInfo ingredient={ingredient} />
+				return <IngredientInfo ingredient={ingredient} updateIngredient={this.updateIngredient}/>
 			})
 
 		}
 
 		return(
+			<Segment>
+				<Segment>
+					<Button 
+						onClick={this.toggleAdd}>
 
-			<Card.Group>
-				{ingredientList}
-			</Card.Group>
+						{this.state.addOpen ? 'close.' : 'add ingredient.'}
+
+					</Button>
+
+					{this.state.addOpen ?
+
+						<Form onSubmit={this.createIngredient}>
+							name:
+			            <Form.Input fluid icon='keyboard outline' iconPosition='left' placeholder='name.' type='text' name='name' onChange={this.handleChange}/>
+			            type:
+			            <select name='type' onChange={this.handleChange}>
+			            	<option value='noodle'>noodle.</option>
+			            	<option value='protein'>protein.</option>
+			            	<option value='sauce'>sauce.</option>
+			            	<option value='normal'>normal.</option>
+			            </select>
+			            vegitarian:
+			            <Form.Input fluid type='checkbox' name='vegitarian' onChange={this.handleChange}/>
+			            vegan:
+			            <Form.Input fluid type='checkbox' name='vegan' onChange={this.handleChange}/>
+			            price:
+			            <Form.Input fluid icon='dollar' iconPosition='left' placeholder='price.' type='number' name='price' onChange={this.handleChange}/>
+			            image:
+		               <Form.Input fluid icon='camera' iconPosition='left' type="file" name='image' onChange={this.handleChange}/>
+			            <Button fluid type='sumbit'>submit.</Button>
+		            </Form>
+
+		         :
+
+		         	null
+		         	
+					}
+
+				</Segment>
+				<Card.Group>
+					{ingredientList}
+				</Card.Group>
+			</Segment>
 
 		)
 	}
